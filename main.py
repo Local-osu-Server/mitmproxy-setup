0027cc9e2a4451afed0f47283f667d682ab066c5
jeevan johnson <jeevanjohnson709@gmail.com>
2024-07-01 18:46:30 -0700

    Uploaded Everything Necessary

---
+++ main.py
@@ -0,0 +1,20 @@
+import config
+from mitmproxy import http  # type: ignore
+from mitmproxy.http import Response
+
+class MyMitmproxy:
+    async def request(self, flow: http.HTTPFlow) -> None:
+        if flow.request.pretty_host.endswith(config.TEMPORARY_REDIRECT_DOMAIN):
+            subdomain = flow.request.pretty_host.split(".")[0]
+            location = flow.request.url.replace(
+                f"https://{subdomain}.{config.TEMPORARY_REDIRECT_DOMAIN}",
+                f"http://localhost:{config.OSU_SERVER_PORT}/{subdomain}",
+            )
+
+            flow.response = Response.make(
+                status_code=307,
+                headers={"Location": location},
+            )
+
+
+addons = [MyMitmproxy()]
---
+++ sample.config.py
@@ -0,0 +1,7 @@
+# mitmproxy needs a domain to initially connect to due to DNS issues.
+# We redirect the traffic to a private server domain solely for the purpose of redirecting it back to localhost.
+# This redirection does not interfere with the private server itself.
+TEMPORARY_REDIRECT_DOMAIN = "" # Example: ripple.moe
+
+# Port of the osu! server so that the client can get redirected to it.
+OSU_SERVER_PORT = 8000
